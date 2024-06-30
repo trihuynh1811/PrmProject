e520850181853b8306d26f20bb8d4e988e7e4ed0
Huy Phạm <153051275+Huycute1104@users.noreply.github.com>
2024-06-30 12:37:58 +0700

    logout

--- app/src/main/java/com/example/prmproject/view/User_Page_Activity.java
+++ app/src/main/java/com/example/prmproject/view/User_Page_Activity.java
@@ -1,6 +1,10 @@
 package com.example.prmproject.view;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,10 +16,25 @@ import com.example.prmproject.R;
 
 public class User_Page_Activity extends AppCompatActivity {
 
+    Button logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user_page);
+        logout = findViewById(R.id.btnLogout);
+        logout.setOnClickListener(v -> {
+            new AlertDialog.Builder(User_Page_Activity.this)
+                    .setTitle("Thoát")
+                    .setMessage("Bạn có chắc chắn muốn thoát ứng dụng?")
+                    .setPositiveButton("Thoát", (d, w) -> {
+                        Intent intent = new Intent(User_Page_Activity.this, LoginActivity.class);
+                        startActivity(intent);
+                        finish();
+                    })
+                    .setNegativeButton("Huỷ", null)
+                    .show();
+        });
+
     }
 }
