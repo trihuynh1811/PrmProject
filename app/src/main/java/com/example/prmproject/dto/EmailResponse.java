1a8b75c66194fa1cdafdf4749fb03f987acabff5
Huy Phạm <153051275+Huycute1104@users.noreply.github.com>
2024-07-03 12:14:32 +0700

    send mail

---
+++ app/src/main/java/com/example/prmproject/dto/EmailResponse.java
@@ -0,0 +1,13 @@
+package com.example.prmproject.dto;
+
+public class EmailResponse {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
